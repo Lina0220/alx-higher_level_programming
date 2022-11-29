819a0e0a9637ef342f6f922d7d1039303b080ad5
Lina0220 <linuaman2017@gmail.com>
2022-11-30 12:38:00 +1400

    done

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-fetch form a give website and display in a given format
+    Script that fetches https://intranet.htbtn.io/status
 """
+
 import requests
 
+
 if __name__ == "__main__":
-    res = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
